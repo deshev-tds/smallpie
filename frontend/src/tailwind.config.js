42ee665df47a6a3e7540f659a6dc300879e67d58
deshev-tds <deshev.tds@gmail.com>
2025-11-15 13:48:44 +0200

    Create tailwind.config.js + some random styling

---
+++ frontend/src/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./main.js"],
+  theme: {
+    extend: {
+      colors: {
+        pipe: {
+          bg: "#0f0f11",
+          card: "#1b1b1f",
+          accent: "#6f9cff",
+          accent2: "#46ffda",
+        }
+      }
+    }
+  },
+  plugins: [],
+};
