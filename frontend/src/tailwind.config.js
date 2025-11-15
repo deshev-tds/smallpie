f21922bcbb60ef9136a1231f9a671ce5709409ee
deshev-tds <deshev.tds@gmail.com>
2025-11-15 14:40:14 +0200

    Update tailwind.config.js to cover "./**/*.{js,ts,jsx,tsx,html}"

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx,html}"
   ],
   theme: {
     extend: {
